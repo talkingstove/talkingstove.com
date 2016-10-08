4a5f2e0733a29c46f26e13fd1e477b4b7d07ec04
Ryan Moore <talkingstove@gmail.com>
2016-10-08 13:45:59 -0700

    get and set

--- js/nagisa/nagisa_config.js
+++ js/nagisa/nagisa_config.js
@@ -31,12 +31,66 @@ N.Classes.NBase = Fiber.extend(function(base) {
   }
 });
 
+N.Library = N.Library || {};
+N.Library.Models = N.Library.Models || {};
+
+// var m = new N.Classes.NModel({name: 'ry'})
+//m.createBinding('testname', '#jt_name');
+//m.set('testname', 'lol')
 N.Classes.NModel = N.Classes.NBase.extend(function(base) {
   return {
     // The `init` method serves as the constructor.
     init: function(params) {
-        // Insert private functions here
-        console.log('NEW model:', params);
+      // Insert private functions here
+      console.log('NEW model:', params);
+      if (!params.name) {
+        console.warn('Model needs a name');
+      }
+      else if ( N.Library.Models[params.name] ) {
+        console.warn('Model with this name already exists... would be overwritten');
+      }
+      else {
+        N.Library.Models[params.name] = this;
+      }
+
+      this.bindings = {};
+    },
+
+    set: function(paramName, value, isSilent) {
+      this[paramName] = value;
+      isSilent = isSilent || false;
+
+      if (!this.bindings[paramName]) {
+        this.bindings[paramName] = {};
+      }
+      else { //update the DOM based on the bindings
+        if (!isSilent) {
+          if (this.bindings[paramName].selector) {
+            $(this.bindings[paramName].selector).val(value); //todo: some fields wont take "val"
+          }
+        }
+      }
+    },
+
+    get: function(paramName) {
+      return this[paramName];
+    },
+
+    //selector = jquery string, eg '#myfield'
+    createBinding: function(paramName, selector) {
+      var self = this;
+
+      if (!this.bindings[paramName]) {
+        this.bindings[paramName] = {};
+      }
+
+      this.bindings[paramName].selector = selector; //todo handle multi
+
+      //create reverse binding from dom to here
+      //watch out for infinite loop!
+      $(selector).on('change', function() {
+        self.set(paramName, $(selector).val(), true);
+      });
     }
   }
 });
@@ -47,7 +101,9 @@ N.Classes.NView = N.Classes.NBase.extend(function(base) {
     init: function(params) {
         // Insert private functions here
         console.log('NEW view:', params);
-    }
+    },
+
+
   }
 });
 
